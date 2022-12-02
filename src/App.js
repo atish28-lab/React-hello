559315eefcf4588b33ecb2e705565ee69c163f3b
atish28-lab <119698769+atish28-lab@users.noreply.github.com>
2022-12-03 00:14:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// Create a functional component called MyProject that returns the following UI:
+
+// An h1 with the name of your project
+// A paragraph to give a brief description of your project
+// An ordered or unordered list to discuss 3 major skills used in this project
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello world project</h1>
+      <p>This is a plain simple project in react</p>
+      <ul>
+        <li>React</li>
+        <li>Codesandbox</li>
+        <li>CodingNinja</li>
+      </ul>
+    </div>
+  );
+}
